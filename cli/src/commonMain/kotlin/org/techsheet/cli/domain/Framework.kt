91525b01bc831b592bb5d5a7100d9ad15234935b
frne <fneff89@gmail.com>
2026-04-17 08:54:28 +0200

    Added specific, unique URLs to all types

--- cli/src/commonMain/kotlin/org/techsheet/cli/domain/Framework.kt
+++ cli/src/commonMain/kotlin/org/techsheet/cli/domain/Framework.kt
@@ -5,37 +5,41 @@ data class Framework(
   val version: String?,
 )
 
-enum class FrameworkType(val title: String, val category: FrameworkCategory) {
+enum class FrameworkType(
+  val title: String,
+  val url: String,
+  val category: FrameworkCategory
+) {
 
   // Application
-  ANGULAR("Angular", FrameworkCategory.APPLICATION),
-  REACT("React", FrameworkCategory.APPLICATION),
-  VUE("Vue.js", FrameworkCategory.APPLICATION),
-  SVELTE("Svelte", FrameworkCategory.APPLICATION),
-  NEXT("Next.js", FrameworkCategory.APPLICATION),
-  EXPRESS("Express", FrameworkCategory.APPLICATION),
-  QT("Qt", FrameworkCategory.APPLICATION),
-  ASP_NET_CORE("ASP.NET Core", FrameworkCategory.APPLICATION),
-  SPRING_BOOT("Spring Boot", FrameworkCategory.APPLICATION),
-  SPRING_MVC("Spring MVC", FrameworkCategory.APPLICATION),
-  SPRING_WEBFLUX("Spring WebFlux", FrameworkCategory.APPLICATION),
-  PLAY_FRAMEWORK("Play Framework", FrameworkCategory.APPLICATION),
-  DJANGO("Django", FrameworkCategory.APPLICATION),
-  FLASK("Flask", FrameworkCategory.APPLICATION),
-  FASTAPI("FastAPI", FrameworkCategory.APPLICATION),
+  ANGULAR("Angular", "https://techsheet.org/framework/angular", FrameworkCategory.APPLICATION),
+  REACT("React", "https://techsheet.org/framework/react", FrameworkCategory.APPLICATION),
+  VUE("Vue.js", "https://techsheet.org/framework/vue", FrameworkCategory.APPLICATION),
+  SVELTE("Svelte", "https://techsheet.org/framework/svelte", FrameworkCategory.APPLICATION),
+  NEXT("Next.js", "https://techsheet.org/framework/nextjs", FrameworkCategory.APPLICATION),
+  EXPRESS("Express", "https://techsheet.org/framework/express", FrameworkCategory.APPLICATION),
+  QT("Qt", "https://techsheet.org/framework/qt", FrameworkCategory.APPLICATION),
+  ASP_NET_CORE("ASP.NET Core", "https://techsheet.org/framework/asp-net-core", FrameworkCategory.APPLICATION),
+  SPRING_BOOT("Spring Boot", "https://techsheet.org/framework/spring-boot", FrameworkCategory.APPLICATION),
+  SPRING_MVC("Spring MVC", "https://techsheet.org/framework/spring-mvc", FrameworkCategory.APPLICATION),
+  SPRING_WEBFLUX("Spring WebFlux", "https://techsheet.org/framework/spring-webflux", FrameworkCategory.APPLICATION),
+  PLAY_FRAMEWORK("Play Framework", "https://techsheet.org/framework/play-framework", FrameworkCategory.APPLICATION),
+  DJANGO("Django", "https://techsheet.org/framework/django", FrameworkCategory.APPLICATION),
+  FLASK("Flask", "https://techsheet.org/framework/flask", FrameworkCategory.APPLICATION),
+  FASTAPI("FastAPI", "https://techsheet.org/framework/fastapi", FrameworkCategory.APPLICATION),
 
   // Data
-  SPRING_DATA("Spring Data", FrameworkCategory.DATA),
+  SPRING_DATA("Spring Data", "https://techsheet.org/framework/spring-data", FrameworkCategory.DATA),
 
   // Security
-  SPRING_SECURITY("Spring Security", FrameworkCategory.SECURITY),
+  SPRING_SECURITY("Spring Security", "https://techsheet.org/framework/spring-security", FrameworkCategory.SECURITY),
 
   // Testing
-  PLAYWRIGHT("Playwright", FrameworkCategory.TESTING),
-  JUNIT("JUnit", FrameworkCategory.TESTING),
+  PLAYWRIGHT("Playwright", "https://techsheet.org/framework/playwright", FrameworkCategory.TESTING),
+  JUNIT("JUnit", "https://techsheet.org/framework/junit", FrameworkCategory.TESTING),
 
   // UI
-  TAILWIND("Tailwind CSS", FrameworkCategory.UI),
+  TAILWIND("Tailwind CSS", "https://techsheet.org/framework/tailwind", FrameworkCategory.UI),
 }
 
 enum class FrameworkCategory(val title: String) {
--- cli/src/commonMain/kotlin/org/techsheet/cli/domain/Tool.kt
+++ cli/src/commonMain/kotlin/org/techsheet/cli/domain/Tool.kt
@@ -5,43 +5,47 @@ data class Tool(
   val version: String?,
 )
 
-enum class ToolType(val title: String, val category: ToolCategory) {
+enum class ToolType(
+  val title: String,
+  val url: String,
+  val category: ToolCategory
+) {
 
   // Build
-  GRADLE("Gradle", ToolCategory.BUILD),
-  GRADLE_KOTLIN("Gradle - Kotlin DSL", ToolCategory.BUILD),
-  GRADLE_GROOVY("Gradle - Groovy DSL", ToolCategory.BUILD),
-  MAVEN("Maven", ToolCategory.BUILD),
-  NPM("NPM", ToolCategory.BUILD),
-  YARN("Yarn", ToolCategory.BUILD),
-  SBT("sbt", ToolCategory.BUILD),
+  GRADLE("Gradle", "https://techsheet.org/tool/gradle", ToolCategory.BUILD),
+  GRADLE_KOTLIN("Gradle - Kotlin DSL", "https://techsheet.org/tool/gradle-kotlin", ToolCategory.BUILD),
+  GRADLE_GROOVY("Gradle - Groovy DSL", "https://techsheet.org/tool/gradle-groovy", ToolCategory.BUILD),
+  MAVEN("Maven", "https://techsheet.org/tool/maven", ToolCategory.BUILD),
+  NPM("NPM", "https://techsheet.org/tool/npm", ToolCategory.BUILD),
+  YARN("Yarn", "https://techsheet.org/tool/yarn", ToolCategory.BUILD),
+  SBT("sbt", "https://techsheet.org/tool/sbt", ToolCategory.BUILD),
 
   // CI
-  GITLAB_CI("GitLab CI", ToolCategory.CI),
-  GITHUB_ACTIONS("GitHub Actions", ToolCategory.CI),
-  CODEOWNERS("CODEOWNERS", ToolCategory.CI),
+  GITLAB_CI("GitLab CI", "https://techsheet.org/tool/gitlab-ci", ToolCategory.CI),
+  GITHUB_ACTIONS("GitHub Actions", "https://techsheet.org/tool/github-actions", ToolCategory.CI),
+  CODEOWNERS("CODEOWNERS", "https://techsheet.org/tool/codeowners", ToolCategory.CI),
 
   // Container
-  DOCKER("Docker", ToolCategory.CONTAINER),
-  DOCKER_COMPOSE("Docker Compose", ToolCategory.CONTAINER),
+  DOCKER("Docker", "https://techsheet.org/tool/docker", ToolCategory.CONTAINER),
+  DOCKER_COMPOSE("Docker Compose", "https://techsheet.org/tool/docker-compose", ToolCategory.CONTAINER),
 
   // Runtime
-  NODE("Node.js", ToolCategory.RUNTIME),
+  NODE("Node.js", "https://techsheet.org/tool/nodejs", ToolCategory.RUNTIME),
 
   // VCS
-  GIT("Git", ToolCategory.VCS),
+  GIT("Git", "https://techsheet.org/tool/git", ToolCategory.VCS),
 
   // Format
-  EDITORCONFIG("EditorConfig", ToolCategory.FORMAT),
-  ESLINT("ESLint", ToolCategory.FORMAT),
-  PRETTIER("Prettier", ToolCategory.FORMAT),
+  EDITORCONFIG("EditorConfig", "https://techsheet.org/tool/editorconfig", ToolCategory.FORMAT),
+  ESLINT("ESLint", "https://techsheet.org/tool/eslint", ToolCategory.FORMAT),
+  PRETTIER("Prettier", "https://techsheet.org/tool/prettier", ToolCategory.FORMAT),
 
   // Security
-  RENOVATE("Renovate", ToolCategory.SECURITY),
+  RENOVATE("Renovate", "https://techsheet.org/tool/renovate", ToolCategory.SECURITY),
 
   // IDE
-  INTELLIJ_IDEA("IntelliJ IDEA", ToolCategory.IDE),
-  VS_CODE("Visual Studio Code", ToolCategory.IDE),
+  INTELLIJ_IDEA("IntelliJ IDEA", "https://techsheet.org/tool/intellij-idea", ToolCategory.IDE),
+  VS_CODE("Visual Studio Code", "https://techsheet.org/tool/vscode", ToolCategory.IDE),
 }
 
 enum class ToolCategory(val title: String) {
