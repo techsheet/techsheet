e5b48e968e407589a09609ba8a25e6a1d1b8fe41
frne <fneff89@gmail.com>
2026-04-23 08:25:55 +0200

    Removed test-projects from main repo

--- cli/src/jvmTest/resources/test-projects/qt-cmake-app/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include <QLabel>
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QLabel label("Hello, TechSheet!");
-    label.show();
-    return app.exec();
-}
